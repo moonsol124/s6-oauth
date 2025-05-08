6736308d1f561effc372c2074f288da808ae1fb8
moonsol124 <moonsol124@gmail.com>
2025-05-08 20:14:09 +0200

    f

--- supabaseClient.js
+++ supabaseClient.js
@@ -2,8 +2,8 @@
 const { createClient } = require('@supabase/supabase-js');
 require('dotenv').config(); // Load environment variables from .env file
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY; // Use the ANON key or SERVICE_ROLE key depending on your setup/RLS
+const supabaseUrl = process.env.SUPABASE_USER_URL;
+const supabaseKey = process.env.SUPABASE_USER_KEY; // Use the ANON key or SERVICE_ROLE key depending on your setup/RLS
 
 if (!supabaseUrl || !supabaseKey) {
     throw new Error("Supabase URL and Key are required. Check environment variables.");
